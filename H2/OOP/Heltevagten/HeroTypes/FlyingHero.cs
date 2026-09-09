bc760876528da8b153295d91412ec9fc6734a297
LuiEgede <lui@gesler.dk>
2026-09-09 10:17:32 +0200

    implement the interfaces

--- H2/OOP/Heltevagten/HeroTypes/FlyingHero.cs
+++ H2/OOP/Heltevagten/HeroTypes/FlyingHero.cs
@@ -1,6 +1,8 @@
+using Heltevagten.Interfaces;
+
 namespace Heltevagten.HeroTypes;
 
-public class FlyingHero : Hero
+public class FlyingHero : Hero, IFlyable
 {
     public FlyingHero(string name, int energy) : base(name, energy)
     {
@@ -10,5 +12,11 @@ public class FlyingHero : Hero
     {
         UseEnergy(10);
         return $"{Name} soars through the sky and rescues people from above.";
+    }
+
+    public string Fly()
+    {
+        UseEnergy(15);
+        return $"{Name} flies swiftly to the incident.";
     }
 }
--- H2/OOP/Heltevagten/HeroTypes/StrongHero.cs
+++ H2/OOP/Heltevagten/HeroTypes/StrongHero.cs
@@ -1,6 +1,8 @@
+using Heltevagten.Interfaces;
+
 namespace Heltevagten.HeroTypes;
 
-public class StrongHero : Hero
+public class StrongHero : Hero, ISuperStrong
 {
     public StrongHero(string name, int energy) : base(name, energy)
     {
@@ -10,5 +12,11 @@ public class StrongHero : Hero
     {
         UseEnergy(15);
         return $"{Name} smashes through obstacles with super strength.";
+    }
+
+    public string LiftHeavyObject()
+    {
+        UseEnergy(10);
+        return $"{Name} lifts the heavy object with incredible strength.";
     }
 }
